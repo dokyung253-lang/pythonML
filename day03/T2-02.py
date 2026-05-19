c912682588ec193a06dba9b16a04fd1f714d450d
dokyung253-lang <dokyung253@gmail.com>
2026-05-19 11:18:14 +0900

    파이썬 2교시  0519

--- day03/T2-02.py
+++ day03/T2-02.py
@@ -46,3 +46,29 @@ print( knr.predict([[100]]) ) # [1010.] # 임의의 물고기 길이 100일 때 
 
 # 문제점 : k-최근접이웃의 문제점은 단순한 주변 이웃의 평균으로 예측하기 때문에 최댓값을 벗어나면 항상 동일한 값으로 예측한다.
 # 즉] 소규모 또는 간단한 예측 프로그램에서만 사용된다.
+
+# [1] (단순)선형회귀 모델 # 1차 방정식
+from sklearn.linear_model import LinearRegression # 선형회귀 모델
+lr = LinearRegression() # 모델 객체 생성
+lr.fit( train_input, train_target ) # 모델 학습
+print( lr.score( test_input, test_target) ) # 모델 평가 
+print( lr.predict( [[50]] )) # 모델 예측 # [1238.3175398]
+print( lr.predict( [[100]] )) # [3191.00026354]
+# 직선 공식(1차 방정식) : y(예측값) = w(가중치)x(특성) + b(절편)
+# 즉] (물고기)무게 = 가중치 * (물고기)길이 + 절편
+print( lr.coef_ )       # 기울기값 반환  [39.05365447]  # 직선의 기울기(특성의 가중치)
+print( lr.intercept_ )  # y절편 반환 -714.3651839448922 # 편향 # x(물고기길이)가 0일 때 y의 값
+# x와 y가 직선관계이며, 실자료들은 물고기 길이가 1씩 증가할 때 무조건 무게 비례증가는 아님. (상관관계 부정확)
+# 즉] 초반에는 길이에 따라 무게가 3배 증가하다가 중후반에는 무게가 2배 혹은 1배 증가할 수 있다. # 사람 : 어릴수록 키 증가 폭 큼, 어느 순간 증가 폭 고정
+
+# [2] 시각화
+import matplotlib.pyplot as plt
+plt.scatter( train_input, train_target ) # x축: 길이, y축 : 무게
+plt.scatter( 50, 1238 ) # 예측된 결과 # 길이가 50일 때 무게는 1238일 것이다.
+plt.scatter( 100, 3191 )
+
+plt.plot( [15, 100], lr.predict([[15], [100]]) ) # 회귀선 그리기 # 15 = (물고기 길이의)시작점, 100 길이의 끝점
+plt.show()
+
+# [3] ( 다항 : 여러개 항 )선형회귀 모델 # 2차 방정식
+#
