de22385393db316a8ed6e91aa35440c50b0198ab
dokyung253-lang <dokyung253@gmail.com>
2026-05-20 18:20:48 +0900

    pr 6

--- day04/service.py
+++ day04/service.py
@@ -1,11 +1,21 @@
 # 서비스
+import pandas as pd
 class Service :
     def __init__(self) :
         self.model = None
 
-    #1. 
     def 학습요청( self, carList ) :
-        
+        # 2
+        df = pd.DataFrame( carList )
+        train_full = df[['평균 연비', '누적 주행거리(km)', '출고 후 경과 월수', '사고 감가 건수', '소유자 변경 횟수']] 
+        train_target = df['매매 가격(단위: 만 원)']
+        from sklearn.model_selection import train_test_split
+        train_input, test_input, train_target, test_target = train_test_split( train_full, train_target, test_size= 0.1, random_state= 42)
+        from sklearn.preprocessing import PolynomialFeatures, StandardScaler
+        from sklearn.linear_model import LinearRegression, Ridge, Lasso
+
+        optimization = []
+
         return True
     # 2. 
     def 예측요청( self, car) :
